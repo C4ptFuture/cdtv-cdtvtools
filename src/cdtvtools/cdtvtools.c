cd8eb313532af3b68ae9d9b91399e3835b5103dc
Captain Future <saftic@icloud.com>
2022-04-24 10:33:46 +0200

    add bookmark store reset option

--- src/cdtvtools/cdtvtools.c
+++ src/cdtvtools/cdtvtools.c
@@ -13,16 +13,26 @@
 
 #define CDTVLAND_MFID 0x434c
 #define CDTVLAND_SYSPREFS_PRODID 0x0001
+#define BOOKMARKMEM 0xDC8000
 
 void usage (void) {
-    printf ("Usage:\n\n CDTVTools [HDDBOOTDELAY <seconds>] [PAL|NTSC]\n\n");
+    printf ("Usage:\n\n CDTVTools [HDDBOOTDELAY <seconds>] [PAL|NTSC] [RESETBMSTORE]\n\n");
+    printf ("  HDDBOOTDELAY seconds             sets HDD Bootdelay to x seconds\n");
+    printf ("  PAL                              force 'PAL' timing (50Hz VBLANK)\n");
+    printf ("  NTSC                             force 'NTSC' timing (60Hz VBLANK)\n");
+    printf ("  RESETBMSTORE                     resets bookmark memory\n\n");
+    printf ("Examples:\n\n");
+    printf ("  CDTVTools HDDBOOTDELAY 10        sets bootdelay to 10 seconds\n");
+    printf ("  CDTVTools HDDBOOTDELAY 10 NTSC   sets bootdelay to 10 sec and forces 60Hz\n");
+    printf ("  CDTVTools RESETBMSTORE           resets bookmark memory\n\n");
 }
 
 int main (int argc, char **argv) 
 {
     
     int i, j;
-    BOOL bootdelay = FALSE;
+    BOOL updbootdelay = FALSE;
+    BOOL resetbmstore = FALSE;
     ULONG seconds, vblanks, bmsize;
     BYTE err;
     UBYTE *data, vblankfreq;
@@ -58,7 +68,7 @@ int main (int argc, char **argv)
     for (i=1; i<argc; i++)
     {
         if (!strcmp(argv[i], "HDDBOOTDELAY")) {
-            bootdelay = TRUE;
+            updbootdelay = TRUE;
             i++;
             if (i >= argc) 
             {
@@ -88,6 +98,9 @@ int main (int argc, char **argv)
         } else if (!strcmp(argv[i], "NTSC")) {
             printf ("\t. 'NTSC' system requested. Will use 60Hz calculation.\n");
             vblankfreq = 60;
+        } else if (!strcmp(argv[i], "RESETBMSTORE")) {
+            printf ("\t. Reset of bookmark memory requested.\n");
+            resetbmstore = TRUE;
         } else if (!strcmp(argv[i], "?")) {
             usage();
             return (0);
@@ -102,97 +115,105 @@ int main (int argc, char **argv)
     }
 
 
-    if (bootdelay) {
-        /* calculate vblanks */
-        vblanks = seconds * vblankfreq;
-        printf ("\t. %d seconds is %d vblanks for %dHz.\n", seconds, vblanks, vblankfreq);
+    if (resetbmstore) {
+        /* write word length to $DC8000 */
+        unsigned short *bookmarkmem = (unsigned short*)BOOKMARKMEM;
+        *bookmarkmem = 0;
+        printf ("\t. Done. Restart your system for bookmark reset to complete.\n");
     }
+    else {
+
+        if (updbootdelay) {
+            /* calculate vblanks */
+            vblanks = seconds * vblankfreq;
+            printf ("\t. %d seconds is %d vblanks for %dHz.\n", seconds, vblanks, vblankfreq);
+        }
 
 
-    /* create IOReq */
-    IOPort = CreatePort(0,0);
-    IOReq = CreateStdIO(IOPort);
+        /* create IOReq */
+        IOPort = CreatePort(0,0);
+        IOReq = CreateStdIO(IOPort);
 
-    /* open bookmark.device */
-    printf ("\t. Opening Bookmark ID %04x.\n", (CDTVLAND_MFID<<16) + CDTVLAND_SYSPREFS_PRODID);
-    err = OpenDevice("bookmark.device", (CDTVLAND_MFID<<16) + CDTVLAND_SYSPREFS_PRODID, IOReq, 0);
-
-    if (!err) {
-        /* check bookmark size */
-        IOReq->io_Command = BD_SIZEOF;
-        IOReq->io_Data = 0;
-        IOReq->io_Offset = 0;
-        IOReq->io_Length = 0;
-        err = DoIO(IOReq);
+        /* open bookmark.device */
+        printf ("\t. Opening Bookmark ID %04x.\n", (CDTVLAND_MFID<<16) + CDTVLAND_SYSPREFS_PRODID);
+        err = OpenDevice("bookmark.device", (CDTVLAND_MFID<<16) + CDTVLAND_SYSPREFS_PRODID, IOReq, 0);
 
         if (!err) {
-            /* alloc mem for bookmark */
-            bmsize = IOReq->io_Actual;
-            data = (UBYTE*)AllocMem(bmsize, MEMF_PUBLIC);
-
-            /* read bookmark */
-            IOReq->io_Command = CMD_READ;
-            IOReq->io_Data = data;
+            /* check bookmark size */
+            IOReq->io_Command = BD_SIZEOF;
+            IOReq->io_Data = 0;
             IOReq->io_Offset = 0;
-            IOReq->io_Length = bmsize;
+            IOReq->io_Length = 0;
             err = DoIO(IOReq);
 
             if (!err) {
-                printf ("\t. Read %lu bytes from bookmark.\n", IOReq->io_Actual);
+                /* alloc mem for bookmark */
+                bmsize = IOReq->io_Actual;
+                data = (UBYTE*)AllocMem(bmsize, MEMF_PUBLIC);
 
-                /* check Riviera header */
-                RivHeader = data;
-                if (RivHeader->magic == 0x52495631) {
-                    Prefs = (struct CDTVLandSysPrefs*) (data+RivHeader->size);
-                    printf ("\t. CDTVLand SysPrefs Bookmark version is %d.\n", Prefs->version);
+                /* read bookmark */
+                IOReq->io_Command = CMD_READ;
+                IOReq->io_Data = data;
+                IOReq->io_Offset = 0;
+                IOReq->io_Length = bmsize;
+                err = DoIO(IOReq);
 
-                    /* Update of HDD bootdelay is requested */
-                    if (bootdelay) {
-                        Prefs->hdd_Bootdelay = vblanks;
+                if (!err) {
+                    printf ("\t. Read %lu bytes from bookmark.\n", IOReq->io_Actual);
 
-                        /* write bookmark */
-                        IOReq->io_Command = CMD_WRITE;
-                        IOReq->io_Data = data;
-                        IOReq->io_Offset = 0;
-                        IOReq->io_Length = bmsize;
-                        err = DoIO(IOReq);
+                    /* check Riviera header */
+                    RivHeader = data;
+                    if (RivHeader->magic == 0x52495631) {
+                        Prefs = (struct CDTVLandSysPrefs*) (data+RivHeader->size);
+                        printf ("\t. CDTVLand SysPrefs Bookmark version is %d.\n", Prefs->version);
 
-                        if (err) {
-                            printf ("ERROR: Could not write to bookmark. Bookmark was NOT updated.\n");
-                        } else {
-                            /* read updated bookmark back */
-                            IOReq->io_Command = CMD_READ;
+                        /* Update of HDD bootdelay is requested */
+                        if (updbootdelay) {
+                            Prefs->hdd_Bootdelay = vblanks;
+
+                            /* write bookmark */
+                            IOReq->io_Command = CMD_WRITE;
                             IOReq->io_Data = data;
                             IOReq->io_Offset = 0;
                             IOReq->io_Length = bmsize;
                             err = DoIO(IOReq);
+
+                            if (err) {
+                                printf ("ERROR: Could not write to bookmark. Bookmark was NOT updated.\n");
+                            } else {
+                                /* read updated bookmark back */
+                                IOReq->io_Command = CMD_READ;
+                                IOReq->io_Data = data;
+                                IOReq->io_Offset = 0;
+                                IOReq->io_Length = bmsize;
+                                err = DoIO(IOReq);
+                            }
+
                         }
 
+                        /* dump current bookmark contents */
+                        printf ("\n\t>> HDD Boot Delay is set to %lu vblanks.\n", Prefs->hdd_Bootdelay);
+                    } else {
+                        printf ("ERROR: Unrecognized bookmark header %08x.\n", RivHeader->magic);
                     }
 
-                    /* dump current bookmark contents */
-                    printf ("\n\t>> HDD Boot Delay is set to %lu vblanks.\n", Prefs->hdd_Bootdelay);
                 } else {
-                    printf ("ERROR: Unrecognized bookmark header %08x.\n", RivHeader->magic);
+                    printf ("ERROR: Could not read bookmark.\n");
                 }
-
             } else {
-                printf ("ERROR: Could not read bookmark.\n");
+                printf ("ERROR: Could not check bookmark size. Error: %d\n", IOReq->io_Error);
             }
+            /* close bookmark.device */
+            CloseDevice(IOReq);
+
         } else {
-            printf ("ERROR: Could not check bookmark size. Error: %d\n", IOReq->io_Error);
+            printf ("ERROR: Could not open bookmark.\n");
         }
-        /* close bookmark.device */
-        CloseDevice(IOReq);
 
-    } else {
-        printf ("ERROR: Could not open bookmark.\n");
+
+        /* Delete IOreq */
+        DeleteStdIO(IOReq);
+        DeletePort(IOPort);
     }
-
-
-    /* Delete IOreq */
-    DeleteStdIO(IOReq);
-    DeletePort(IOPort);
-
     return (0);
 }
